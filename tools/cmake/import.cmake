8391bb0f759379a37e294f57bb97e3c7dcb5a417
Malte Pedersen <maltebp94@gmail.com>
2024-03-05 20:09:48 +0100

    Added tundra CMake import script

---
+++ tools/cmake/import.cmake
@@ -0,0 +1,32 @@
+# This is by no means a proper setup function - I just wanted to move all the Tundra setup out of the
+# project CMakeLists.txt and this will do for now
+function(td_link_engine target) 
+
+    add_compile_definitions(TD_PLATFORM_PLAYSTATION)
+
+    get_filename_component( TD_DIR external/tundra ABSOLUTE)
+    get_filename_component( TD_INCLUDE ${TD_DIR}/include ABSOLUTE)
+    get_filename_component( TD_LIB ${TD_DIR}/build/playstation/lib ABSOLUTE)
+
+    target_include_directories(
+        ${target}
+        PRIVATE ${TD_INCLUDE}/base
+        PRIVATE ${TD_INCLUDE}/playstation/engine
+    )
+
+    add_library(td-engine-debug STATIC IMPORTED)
+    set_target_properties(td-engine-debug PROPERTIES IMPORTED_LOCATION ${TD_LIB}/Debug/libtd.playstation.engine.a )
+
+    add_library(td-engine-release STATIC IMPORTED)
+    set_target_properties(td-engine-release PROPERTIES IMPORTED_LOCATION ${TD_LIB}/Release/libtd.playstation.engine.a )
+
+    add_library(td-base-debug STATIC IMPORTED)
+    set_target_properties(td-base-debug PROPERTIES IMPORTED_LOCATION ${TD_LIB}/Debug/libtd.base.a )
+
+    add_library(td-base-release STATIC IMPORTED)
+    set_target_properties(td-base-release PROPERTIES IMPORTED_LOCATION ${TD_LIB}/Release/libtd.base.a )
+
+    target_link_libraries(tundra-sandbox PRIVATE debug td-engine-debug td-base-debug)
+    target_link_libraries(tundra-sandbox PRIVATE optimized td-engine-release td-base-release)
+
+endfunction()
