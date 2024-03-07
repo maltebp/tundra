37d044149067711ccc15b57f69d8fac308ad76c4
Malte Pedersen <maltebp94@gmail.com>
2024-03-07 17:58:33 +0100

    Added list.hpp + tests

---
+++ include/base/tundra/core/list.hpp
@@ -0,0 +1,112 @@
+#pragma once
+
+#include <tundra/core/types.hpp>
+#include <tundra/core/assert.hpp>
+#include <cstdlib>
+
+// For some reason, it cannot find the placement new
+void* operator new(size_t count, void* address) noexcept;
+
+namespace td {  
+
+    template<typename T>
+    class List {
+    public:
+
+        List() = default;
+
+        List(const List& list) {
+
+        }
+
+        ~List() {
+            if( elements != nullptr ) {
+                for( int i = 0; i < size; i++ ) {
+                    elements[i].~T();
+                }
+                ::operator delete ((void*)elements);
+            }
+        }
+
+        void add(const T& element) {
+            if( size == available_size ) {
+                uint32 new_size = available_size == 0 ? 2 : available_size * 2;
+                reallocate(new_size);
+            }
+
+            new (elements + size) T(element);
+            size++;
+        }
+
+        void remove(uint32 index) {
+            TD_ASSERT(index < size, "List index out of bounds (was %d but size is %d)", index, size);
+            elements[index].~T();
+
+            for( int i = index + 1; i < size; i++ ) {
+                // TODO: Use move constructor instead
+                new(elements + i - 1) T(elements[i]);
+                elements[i].~T();
+            }
+
+            size--;
+        }
+
+        void clear() {
+            if( elements == nullptr ) return;
+
+            for( int i = 0; i < size; i++ ) {
+                elements[i].~T();
+            }            
+
+            ::operator delete ((void*)elements);
+            elements = nullptr;
+            size = 0;
+        }
+
+        void reseve() {
+
+        }
+
+        td::uint32 get_size() const { return size; }
+
+        T& operator[](uint32 i) {
+            TD_ASSERT(i < size, "List index out of bounds (was %d but size is %d)", i, size);
+            return elements[i];
+        }
+
+        const T& operator[](uint32 i) const {
+            TD_ASSERT(i < size, "List index out of bounds (was %d but size is %d)", i, size);
+            return elements[i];
+        }
+
+    private:
+
+        void reallocate(uint32 new_available_size) {
+            TD_ASSERT(new_available_size != 0, "Cannot allocate 0 elements (mplementation bug in td::List)");
+            TD_ASSERT(new_available_size > available_size, "Cannot reallocate to smaller size (current size is %d, new size %d) (implementation bug in td::List )", available_size, new_available_size);
+
+            T* new_elements = (T*) ::operator new(sizeof(T) * new_available_size);
+            TD_ASSERT(new_elements != nullptr, "Failed at allocated buffer of size %d", new_available_size * sizeof(T));
+
+            if( elements != nullptr ) {
+                for( int i = 0; i < size; i++ ) {
+                    new (new_elements + i) T(elements[i]);
+                    elements[i].~T();
+                }
+
+                ::operator delete ((void*)elements);    
+            }
+            
+            elements = new_elements;
+            available_size = new_available_size;
+        }
+
+        T* elements = nullptr;
+
+        td::uint32 available_size = 0;
+
+        td::uint32 size = 0;
+
+    };
+
+}
---
+++ tests/base/core/list.tests.hpp
@@ -0,0 +1,162 @@
+#pragma once
+
+#include "tundra/core/string-util.hpp"
+#include "tundra/core/string.hpp"
+#include <tundra/test-framework/test.hpp>
+#include <tundra/test-framework/test-assert.hpp>
+
+#include <tundra/core/list.hpp>
+
+
+class TestType {
+public:
+
+    TestType() {
+        num_constructors_called++;
+    }
+
+    TestType(int value) : value(value) {
+        num_constructors_called++;
+    }
+
+    TestType(const TestType& other) 
+        :   value(other.value) {
+        num_constructors_called++;
+    }
+
+    ~TestType() {
+        num_destructors_called++;
+    }
+
+    bool operator==(const TestType& other) const {
+        return value == other.value;
+    }
+
+    bool operator!=(const TestType& other) const {
+        return value != other.value;
+    }
+
+    int value = 0;
+
+    static inline td::uint32 num_constructors_called = 0;
+    static inline td::uint32 num_destructors_called = 0;
+};
+
+template<>
+td::String td::to_string<TestType>(const TestType& t) {
+    return td::string_util::create_from_format("TestType(%d)", t.value);
+}
+
+TD_TEST("list/add") {
+    
+    TestType t1 { 10 };
+
+    TestType::num_constructors_called = 0;
+    TestType::num_destructors_called = 0;
+
+    td::List<TestType> list;
+    list.add(t1);
+
+    TD_TEST_ASSERT_EQUAL(TestType::num_constructors_called, 1);
+    TD_TEST_ASSERT_EQUAL(TestType::num_destructors_called, 0);
+
+    TD_TEST_ASSERT_EQUAL(list[0], TestType{10});    
+    TD_TEST_ASSERT_EQUAL(list.get_size(), 1);   
+}
+
+TD_TEST("list/add-and-remove") {
+    
+    TestType t1 { 10 };
+
+    TestType::num_constructors_called = 0;
+    TestType::num_destructors_called = 0;
+
+    td::List<TestType> list;
+    list.add(t1);
+    list.remove(0);
+
+    TD_TEST_ASSERT_EQUAL(TestType::num_destructors_called, 1);
+    TD_TEST_ASSERT_EQUAL(list.get_size(), 0);       
+}
+
+TD_TEST("list/destructor") {
+    TestType t1 { 10 };
+
+    TestType::num_constructors_called = 0;
+    TestType::num_destructors_called = 0;
+
+    {
+        td::List<TestType> list;
+        list.add(t1);    
+    }
+    
+    TD_TEST_ASSERT_EQUAL(TestType::num_destructors_called, 1);
+}
+
+TD_TEST("list/clear") {
+    
+    TestType t1 { 10 };
+    TestType t2 { 15 };
+
+    TestType::num_constructors_called = 0;
+    TestType::num_destructors_called = 0;
+
+    td::List<TestType> list;
+    list.add(t1);
+    list.add(t2);
+
+    list.clear();
+
+    TD_TEST_ASSERT_EQUAL(list.get_size(), 0);
+    TD_TEST_ASSERT_EQUAL(TestType::num_destructors_called, 2);
+}
+
+TD_TEST("list/clear-reuse") {
+    
+    TestType t1 { 10 };
+    TestType t2 { 15 };
+    TestType t3 { 20 };
+    TestType t4 { 25 };
+
+    TestType::num_constructors_called = 0;
+    TestType::num_destructors_called = 0;
+
+    td::List<TestType> list;
+    list.add(t1);
+    list.add(t2);
+    list.clear();
+
+    list.add(t1);
+    list.add(t2);
+    
+    TD_TEST_ASSERT_EQUAL(list.get_size(), 2);
+    TD_TEST_ASSERT_EQUAL(TestType::num_constructors_called, 4);
+    TD_TEST_ASSERT_EQUAL(TestType::num_destructors_called, 2);
+}
+
+TD_TEST("list/add-with-relocation") {
+
+    TestType t1 { 10 };
+    TestType t2 { 15 };
+    TestType t3 { 20 };
+
+    TestType::num_constructors_called = 0;
+    TestType::num_destructors_called = 0;
+
+    td::List<TestType> list;
+    list.add(t1);
+    list.add(t2);
+
+    TD_TEST_ASSERT_EQUAL(TestType::num_constructors_called, 2);
+
+    list.add(t3);
+
+    TD_TEST_ASSERT_EQUAL(list.get_size(), 3);
+
+    TD_TEST_ASSERT_EQUAL(TestType::num_constructors_called, 5);
+    TD_TEST_ASSERT_EQUAL(TestType::num_destructors_called, 2);
+
+    TD_TEST_ASSERT_EQUAL(list[0], TestType{10});
+    TD_TEST_ASSERT_EQUAL(list[1], TestType{15});
+    TD_TEST_ASSERT_EQUAL(list[2], TestType{20});
+}
