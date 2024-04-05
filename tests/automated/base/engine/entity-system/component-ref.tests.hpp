23424f8474cb6c27a132ddd12753256e8eb7950a
Malte Pedersen <maltebp94@gmail.com>
2024-04-05 14:41:30 +0200

    Added some more tests for component ref

--- tests/automated/base/engine/entity-system/component-ref.tests.hpp
+++ tests/automated/base/engine/entity-system/component-ref.tests.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "tundra/core/assert.hpp"
+#include "tundra/core/string-util.hpp"
 #include <tundra/test-framework/test.hpp>
 #include <tundra/test-framework/test-assert.hpp>
 #include <tundra/test-framework/utility/constructor-statistics.hpp>
@@ -10,6 +11,14 @@
 #include <tundra/engine/entity-system/entity.hpp>
 
 namespace td::component_ref_tests {
+    class TestComponent;
+}
+
+template<>
+td::String td::to_string<td::component_ref_tests::TestComponent>(const td::component_ref_tests::TestComponent& c);
+
+namespace td::component_ref_tests {
+    
 
     class TestComponent : public Component<TestComponent>, public ConstructorStatistics {
     public: 
@@ -17,6 +26,8 @@ namespace td::component_ref_tests {
         int32 a { 1 };
         int16 b { 2};
         int8 c { 3};
+
+        constexpr bool operator==(const TestComponent& other) const { return a == other.a && b == other.b && c == other.c; }
     };
     
     TD_TEST("engine/entity-system/component-ref/basic") {
@@ -24,15 +35,32 @@ namespace td::component_ref_tests {
         
         Entity* e = Entity::create();
         
-        ComponentRef<TestComponent> c_ref = e->add_component<TestComponent>();
+        TestComponent* c = e->add_component<TestComponent>();
+        ComponentRef<TestComponent> c_ref = c;
 
         TestComponent* c_deref = c_ref;
-        TD_TEST_ASSERT_NOT_EQUAL(c_deref, nullptr);
+        TD_TEST_ASSERT_EQUAL(c_deref, c);
+        TD_TEST_ASSERT_EQUAL(&*c_ref, c);
+
         TD_TEST_ASSERT_EQUAL(c_deref->a, 1);
         TD_TEST_ASSERT_EQUAL(c_deref->b, 2);
         TD_TEST_ASSERT_EQUAL(c_deref->c, 3);
 
         TD_TEST_ASSERT_EQUAL(TestComponent::num_constructors_called, 1U);
+    }
+
+    TD_TEST("engine/entity-system/component-ref/copy-constructor") {
+        TestComponent::reset_constructor_counters();
+        Entity* e = Entity::create();
+        TestComponent* c = e->add_component<TestComponent>();
+
+        ComponentRef<TestComponent> c_ref_1 = c;
+        ComponentRef<TestComponent> c_ref_2 = c_ref_1;
+
+        TD_TEST_ASSERT_EQUAL(c_ref_1, c_ref_2);
+
+        TD_TEST_ASSERT_EQUAL(*c_ref_1, TestComponent{});
+        TD_TEST_ASSERT_EQUAL(*c_ref_2, TestComponent{});
     }
 
     TD_TEST("engine/entity-system/component-ref/equality") {
@@ -155,4 +183,9 @@ namespace td::component_ref_tests {
         e->destroy();
     }
 
+}
+
+template<>
+td::String td::to_string<td::component_ref_tests::TestComponent>(const td::component_ref_tests::TestComponent& c) {
+    return td::string_util::create_from_format("TestComponent{%d, %d, %d}", c.a, c.b, c.c);
 }
